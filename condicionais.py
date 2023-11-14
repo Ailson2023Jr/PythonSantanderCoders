0ed13bf3d39cc1c21658312c6b0bafe6b70e0b15
Junior <ailsonferreirajr@gmail.com>
2023-11-14 11:42:34 -0300

    lista

--- condicionais.py
+++ condicionais.py
@@ -18,12 +18,14 @@
 #     print('Você foi REPROVADO (0)')   
 
 
-media = 7
-presenca =100
+# media = 7
+# presenca =100
 
-if media >= 7 and presenca >= 70:
-  print('Aprovado')
-  print('Parabéns')
-else:
-  print('Reprovado')
-  print('Tente outra vez')  
+# if media >= 7 and presenca >= 70:
+#   print('Aprovado')
+#   print('Parabéns')
+# else:
+#   print('Reprovado')
+#   print('Tente outra vez')  
+
+
---
+++ while.py
@@ -0,0 +1,29 @@
+numero_sorteado = 15
+
+numero_escolhido = int(input('Informe um número entre 1 e 20: '))
+while numero_escolhido != numero_sorteado:
+  print('Errou, Tente novamente')
+
+  numero_escolhido = int(input('Informe um número entre 1 e 20:'))
+
+print ('Parabéns você acertou')
+
+
+
+
+
+# if numero_sorteado == numero_escolhido:
+#   print('Você acertou!')
+# else:
+#   print('Você errou!')  
+
+# #  Exemplo 02: Estrutura com contador
+
+
+# contador = 0
+
+# while contador < 5:
+#   print(contador)
+
+#   contador = contador + 1
+  
